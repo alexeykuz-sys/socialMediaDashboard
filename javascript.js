3e83ea14d299afcf6898bce2f87741f2255f1e49
unknown <alexey kuzmis>
2020-12-18 13:44:59 +0000

    applied dark mode to body and cards

--- javascript.js
+++ javascript.js
@@ -1,26 +1,11 @@
 
 let switchButton = document.getElementById('switch'); 
-let modeLight = document.getElementById('light')
-let modeDark = document.getElementById('dark')
+let modeLight = document.getElementById('light-button')
+let modeDark = document.getElementById('dark-button')
 const slider = document.getElementById('slider')
 let wrapper = document.getElementsByClassName('card-wrapper')
-console.log(wrapper)
 
 
-
-let el = document.querySelectorAll('.light')
-console.log(typeof('el'))
-
-let elements = [];
-let classNames = [];
-
-function addClass(elements, classNames) {
-
-    if (!(elements instanceof String)) {
-        elements.classList.add(classNames);
-        return;
-    }
-}
 
 
 function changeMode(){
@@ -29,86 +14,53 @@ function changeMode(){
         modeLight.classList.add('none')
         modeDark.classList.add('show')
         slider.style.backgroundColor = 'var(--LimeGreen)'
-        
+        bodyModeChange()
 
     } else if (modeDark.classList.contains('show')){
         modeDark.classList.remove('show')
         modeLight.classList.remove('none')
         modeLight.classList.add('show')
         slider.style.backgroundColor = '#ccc'
-        
+        bodyModeChange()
     }
+
     console.log(modeDark, modeLight)
 }
-switchButton.addEventListener('change', changeMode)
+switchButton.addEventListener('change', changeMode, bodyModeChange)
+console.log(modeDark, modeLight)
 
-function changeModeCard(status){
-for (i=0; i<el.length; i++){
-    let elements = ['class-inner']
-    let classNames = ['dark']
-    if(status = document.classList.contains('light') ){
+
+function bodyModeChange (){
+    if(modeDark.classList.contains('show')){
+        document.body.style.backgroundColor = 'var(--VeryDarkBlueBG)'
+        darkCardMode()
+        
+    } else if (modeLight.classList.contains('show')){
+        document.body.style.backgroundColor = 'var(--WhiteBG)'
+        lightCardMode()
         
     }
 }
-}
-        
+
+function lightCardMode(){
+    let card =  document.getElementsByClassName('card-inner')
+    let card2 =  document.getElementsByClassName('card-inner-2')
     
+    for (let i=0;i<card.length; i++){
+        card[i].style.backgroundColor = 'var(--VeryPaleBlueTopBGPattern)'
+    }
+    for (let i=0;i<card2.length; i++){
+        card2[i].style.backgroundColor = 'var(--VeryPaleBlueTopBGPattern)'
+    }
+}
 
-
-
-// function changeMode(){
-//     if(modeLight.classList.contains('show')){
-//         modeLight.classList.remove('show')
-//         modeLight.classList.add('none')
-//         modeDark.classList.add('show')
-//         slider.style.backgroundColor = 'var(--LimeGreen)'
-        
-
-//     } else if (modeDark.classList.contains('show')){
-//         modeDark.classList.remove('show')
-//         modeLight.classList.remove('none')
-//         modeLight.classList.add('show')
-//         slider.style.backgroundColor = '#ccc'
-        
-//     }
-//     console.log(modeDark, modeLight)
-// }
-// switchButton.addEventListener('change', changeMode)
-
-// let elements = [];
-// let classNames = [];
-
-// function addClass(elements, classNames) {
-
-//     if (!(elements instanceof Array)) {
-//         elements.classList.add(classNames);
-//         return;
-//     }
-// }
-
-// function removeClass(elements, classNames) {
-
-//     if (!(elements instanceof Array)) {
-//         elements.classList.remove(classNames);
-//         return;
-//     }
-
-//     for (let element of elements) {
-//         for (let name of classNames) {
-//             element.classList.remove(name)
-//         }
-//     }
-// }
-
-// function LightDark(){
-//     console.log(modeDark, modeLight)
-//     if(modeLight.classList.contains('show')){
-//         elements=['body']
-//         classNames = ['dark']
-//         addClass(elements, classNames)
-//         console.log(status)
-//         removeClass(elements)
-//     };
-// console.log('a')
-// };
-// LightDark()
+function darkCardMode(){
+    let card =  document.getElementsByClassName('card-inner')
+    let card2 =  document.getElementsByClassName('card-inner-2')
+    for (let i=0;i<card.length; i++){
+        card[i].style.backgroundColor = 'var(--DarkDesaturtedBlueCardBG)'
+    }
+    for (let i=0;i<card2.length; i++){
+        card2[i].style.backgroundColor = 'var(--DarkDesaturtedBlueCardBG)'
+    }
+}
